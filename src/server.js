ae3fd43d9169d154120de89e659890c3cbb7274d
sanstha1 <sthasantosh070@gmail.com>
2026-06-28 13:40:58 +0545

    add initial server and app setup with routes and middleware

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/database');
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+start();
